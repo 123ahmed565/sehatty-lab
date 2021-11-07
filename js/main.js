6926e7f8518716721aa682208c857a004f522921
123ahmed565 <ahmedseragmohamed@gmail.com>
2021-11-07 15:32:48 +0200

    home page & about page & contact page & medical test page and blog page

---
+++ js/main.js
@@ -0,0 +1,14 @@
+ // testimonials
+ var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop:true,
+    autoplay:true,
+    freeMode: true,
+    grabCursor:true,
+    loop:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
