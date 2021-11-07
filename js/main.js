efd2192834bad80ba490c4018c05e726d3dffb55
123ahmed565 <ahmedseragmohamed@gmail.com>
2021-11-07 16:52:36 +0200

    add soome divs and some touches

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
+// swiper slider for departments
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4.5,
+  spaceBetween: 20,
+  freeMode: true,
+  grabCursor:true,
+  loop:true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints:{
+    0: {
+      slidesPerView: 2.5,
+      spaceBetween: 15
+  
+    },
+    600: {
+      slidesPerView: 3.5,
+      spaceBetween: 20
+    },
+    1000: {
+      slidesPerView: 4.5,
+      spaceBetween: 20
+    },
+      
+  }
+}); 
+ 
+ 
+ 
+ 
  // testimonials
  var swiper = new Swiper(".mySwiper2", {
     slidesPerView: 1,
